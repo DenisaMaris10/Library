5d664d018a7fa2360fa9a22c82ba9a557704878e
Denisa Maris <maris.em.denisa@student.utcluj.ro>
2024-11-21 22:51:38 +0200

    Added Singleton for application dependencies

--- src/main/java/launcher/LoginComponentFactory.java
+++ src/main/java/launcher/LoginComponentFactory.java
@@ -8,6 +8,8 @@ import repository.security.RightsRolesRepository;
 import repository.security.RightsRolesRepositoryMySQL;
 import repository.user.UserRepository;
 import repository.user.UserRepositoryMySQL;
+import service.book.BookService;
+import service.book.BookServiceImpl;
 import service.user.AuthenticationService;
 import service.user.AuthenticationServiceImpl;
 import view.LoginView;
@@ -16,33 +18,41 @@ import java.sql.Connection;
 
 public class LoginComponentFactory {
     private final LoginView loginView;
-    //private final LoginController loginController;
+    private final LoginController loginController;
     private final AuthenticationService authenticationService;
     private final UserRepository userRepository;
     private final RightsRolesRepository rightsRolesRepository;
-    private final BookRepositoryMySQL bookRepository;
-    private static LoginComponentFactory instance;
+    private final BookRepository bookRepository;
+    private final BookService bookService;
+    private static volatile LoginComponentFactory instance;
     private static Boolean componentsForTests;
     private static Stage stage;
 
     public static LoginComponentFactory getInstance(Boolean aComponentsForTests, Stage aStage) {
+
         if (instance == null) {
-            componentsForTests = aComponentsForTests;
-            stage = aStage;
-            instance = new LoginComponentFactory(componentsForTests, stage);
+
+            synchronized (LoginComponentFactory.class) {
+                if (instance == null) {
+                    componentsForTests = aComponentsForTests;
+                    stage = aStage;
+                    instance = new LoginComponentFactory(componentsForTests, stage);
+                }
+            }
         }
 
         return instance;
     }
 
-    public LoginComponentFactory(Boolean componentsForTests, Stage stage){
+    private LoginComponentFactory(Boolean componentsForTests, Stage stage){
         Connection connection = DatabaseConnectionFactory.getConnectionWrapper(componentsForTests).getConnection();
         this.rightsRolesRepository = new RightsRolesRepositoryMySQL(connection);
         this.userRepository = new UserRepositoryMySQL(connection, rightsRolesRepository);
         this.authenticationService = new AuthenticationServiceImpl(userRepository, rightsRolesRepository);
         this.loginView = new LoginView(stage);
-        //this.loginController = new LoginController(loginView, authenticationService);
+        this.loginController = new LoginController(loginView, authenticationService);
         this.bookRepository = new BookRepositoryMySQL(connection);
+        this.bookService = new BookServiceImpl(bookRepository);
     }
 
     public static Stage getStage(){
@@ -69,12 +79,12 @@ public class LoginComponentFactory {
         return loginView;
     }
 
-    public BookRepositoryMySQL getBookRepository(){
+    public BookRepository getBookRepository(){
         return bookRepository;
     }
 
-//    public LoginController getLoginController(){
-//        return loginController;
-//    }
+    public LoginController getLoginController(){
+        return loginController;
+    }
 
 }
